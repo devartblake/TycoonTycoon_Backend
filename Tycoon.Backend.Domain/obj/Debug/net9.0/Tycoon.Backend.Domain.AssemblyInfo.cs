73831f535bbb1e7aee2630d7d8d11f61ff5e66d3
LMX Blade <devart.blake@gmail.com>
2026-01-10 01:38:47 -0500

    Update assembly informational version hashes
    Regenerated AssemblyInfo and cache files to update the AssemblyInformationalVersionAttribute hash across all projects. No functional code changes included.

--- Tycoon.Backend.Domain/obj/Debug/net9.0/Tycoon.Backend.Domain.AssemblyInfo.cs
+++ Tycoon.Backend.Domain/obj/Debug/net9.0/Tycoon.Backend.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tycoon.Backend.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9f8b73f180912d428a45d0cc46c7caa272429a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7091690192bf0fd133904899e7f62aaac9e522b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tycoon.Backend.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tycoon.Backend.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
