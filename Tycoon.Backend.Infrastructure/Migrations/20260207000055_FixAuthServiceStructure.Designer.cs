377c8f30f032fe40ab7904e4ad89cf868e93a8e2
LMX Blade <devart.blake@gmail.com>
2026-02-07 12:33:20 -0500

    Bump NuGetToolVersion to 6.14.2
    Update generated *.csproj.nuget.g.props files across multiple projects to set NuGetToolVersion from 6.14.0 to 6.14.2. These changes reflect an upgrade of the NuGet tooling used during restore/build and were applied to the obj-level generated props for the affected projects.

--- Tycoon.Backend.Infrastructure/Migrations/20260207000055_FixAuthServiceStructure.Designer.cs
+++ Tycoon.Backend.Infrastructure/Migrations/20260207000055_FixAuthServiceStructure.Designer.cs
@@ -2,18 +2,21 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Tycoon.Backend.Infrastructure.Persistence;
 
 #nullable disable
 
-namespace Tycoon.Backend.Infrastructure.Persistence.Migrations
+namespace Tycoon.Backend.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDb))]
-    partial class AppDbModelSnapshot : ModelSnapshot
+    [Migration("20260207000055_FixAuthServiceStructure")]
+    partial class FixAuthServiceStructure
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
